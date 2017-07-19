478223e636fb2b084ef7c1ede199c0beabad61c1
Jess <a.kravchenko357@gmail.com>
2017-07-19 20:40:03 +0300

    from master allure branch

---
+++ src/test/java/Test1.java
@@ -0,0 +1,30 @@
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Step;
+
+/**
+ * Created by Dell-SSD on 19.07.2017.
+ */
+public class Test1 {
+    @Test
+    public void azaza(){
+        sendEmail();
+        logIn();
+        sendEmail();
+        logIn();
+
+    }
+    @Test
+    public void atata(){
+        logIn();
+        sendEmail();
+    }
+    @Step("Почта")
+    public void sendEmail(){
+
+    }
+
+    @Step ("LogIn")
+    public void logIn(){
+
+    }
+}
