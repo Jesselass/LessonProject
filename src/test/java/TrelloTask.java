bceaff1c54a763f76f54129d1f5565d7554a7e4d
Jess <a.kravchenko357@gmail.com>
2017-07-31 21:02:15 +0300

    from master allure branch

--- src/test/java/TrelloTask.java
+++ src/test/java/TrelloTask.java
@@ -48,9 +48,8 @@ public class TrelloTask extends BrowserFactory {
         wait.until(ExpectedConditions.urlToBe("https://trello.com/b/pYbUadku/testboard"));
         driver().findElement(By.cssSelector(".placeholder")).click();
         driver().findElement(By.cssSelector(".list-name-input")).sendKeys("NewList");
-       WebElement element = driver().findElement(By.cssSelector(".js-save-edit"));
-        
-        element.click();
+        driver().findElement(By.cssSelector(".js-save-edit")).click();
+
         String check = driver().findElement(By.cssSelector("[aria-label=NewList]")).getText();
 
         Assert.assertTrue(check.equals("NewList"),"LIST CREATION FAILED");
