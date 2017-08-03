6131511bb83e02e94d7294b7d6e8a16f7049bcec
Jess <a.kravchenko357@gmail.com>
2017-08-03 15:12:49 +0300

    from master allure branch

---
+++ src/main/java/finalTest/PDfiller.java
@@ -0,0 +1,100 @@
+package finalTest;
+
+import core.BrowserFactory;
+import core.Elem;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+/**
+ * Created by Dell-SSD on 02.08.2017.
+ */
+public class PDfiller extends BrowserFactory {
+    List<WebElement> fileNames = new ArrayList<>();
+    public String check ="";
+    @Test
+    @Parameters({"email", "pass"})
+    public void logIn(String email, String pass) {
+        get("https://www.pdffiller.com/");
+        driver().manage().window().maximize();
+        driver().findElement(By.cssSelector("[href='/en/login.htm']")).click();
+        driver().findElement(By.cssSelector("#form-login-email")).clear();
+        driver().findElement(By.cssSelector("#form-login-email")).sendKeys(email);
+        driver().findElement(By.cssSelector("#form-login-password")).clear();
+        driver().findElement(By.cssSelector("#form-login-password")).sendKeys(pass);
+        driver().findElement(By.cssSelector("#form-login-submit")).click();
+        WebDriverWait wait = new WebDriverWait(driver(), 20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".mf-icon-mybox")));
+        Assert.assertTrue(driver().getCurrentUrl().equals("https://www.pdffiller.com/en/forms.htm"));
+    }
+
+    @Test(dependsOnMethods = "logIn")
+    public void copyFile() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver(), 20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".mf-doc__item .checkbox")));
+        Actions actions = new Actions(driver());
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".g-ibtn.i-copy.tooltipstered")));
+        WebElement copyIcon = driver().findElement(By.cssSelector(".g-ibtn.i-copy.tooltipstered"));
+        actions.moveToElement(copyIcon).click().perform();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[placeholder='Form Name']")));
+        driver().findElement(By.cssSelector("[placeholder='Form Name']")).clear();
+        driver().findElement(By.cssSelector("[placeholder='Form Name']")).sendKeys("CopyFile");
+        driver().findElement(By.cssSelector("div >.btn.-sm.-orange")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".mf-info-banner__button")));
+        fileNames = driver().findElements(By.cssSelector(".mf-doc__name span"));
+
+        for (WebElement name : fileNames) {
+            if (name.getText().equals("CopyFile")) {
+                check = name.getText();
+            }
+        }
+        Assert.assertTrue(check.equals("CopyFile"));
+    }
+
+    @Test(dependsOnMethods = "copyFile")
+    public void deleteFile() throws TimeoutException {
+        WebDriverWait wait = new WebDriverWait(driver(), 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".g-ibtn.i-trash-bin.tooltipstered")));
+        Actions actions = new Actions(driver());
+        WebElement deleteIcon = driver().findElement(By.cssSelector(".g-ibtn.i-trash-bin.tooltipstered"));
+        actions.moveToElement(deleteIcon).click().perform();
+        wait.until(visibilityOfElementLocated(By.cssSelector(".notification.notification-success")));
+        driver().findElement(By.cssSelector(".notification.notification-success")).click();
+        /* try{
+            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("mf-info-banner__close")));
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+*/
+        fileNames = driver().findElements(By.cssSelector(".mf-doc__name span"));
+        for (WebElement name : fileNames) {
+            if (name.getText().equals(check)) {
+                check = "Element is here";
+                System.out.println(check);
+            }
+        }
+
+       Assert.assertFalse(check.equals("Element is here"));
+
+    }
+    @Test(dependsOnMethods = "deleteFile")
+    public void logOut(){
+        WebDriverWait wait = new WebDriverWait(driver(), 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".h-ac__toggle")));
+        driver().findElement(By.cssSelector(".h-ac__toggle")).click();
+        driver().findElement(By.cssSelector("[href='/en/logout.htm']")).click();
+        Assert.assertTrue(driver().getCurrentUrl().equals("https://www.pdffiller.com/en/login.htm?msg=logout"));
+    }
+}
--- src/test/java/TrelloTask.java
+++ src/test/java/TrelloTask.java
@@ -4,6 +4,7 @@ import core.Elem;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,6 +14,7 @@ import org.testng.annotations.Test;
 import trelloApi.TrelloApiClient;
 import trelloApi.models.Board;
 import trelloApi.models.TrelloCard;
+import trelloApi.models.TrelloList;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,10 +23,16 @@ import java.util.List;
  * Created by Dell-SSD on 31.07.2017.
  */
 public class TrelloTask extends BrowserFactory {
+   public String boardId = "pYbUadku";
+    public String listId = "";
+    public TrelloApiClient trelloApiClient = new TrelloApiClient();
+
+
+
     @Test
     @Parameters({"email", "pass"})
-    public void logIn(String userEmail,String pass) throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver(), 10);
+    public void logIn(String userEmail,String pass)  {
+        WebDriverWait wait = new WebDriverWait(driver(), 15);
         get("https://trello.com/");
         driver().findElement(By.cssSelector(".global-header-section-button")).click();
         driver().findElement(By.cssSelector("#user")).clear();
@@ -58,42 +66,47 @@ public class TrelloTask extends BrowserFactory {
     }
 
     @Test (dependsOnMethods = "createList")
-    public void createCard(){
+    public void createCard() throws IOException {
         driver().findElement(By.cssSelector(".open-card-composer")).click();
         driver().findElement(By.cssSelector(".js-card-title")).sendKeys("azaza karto4ka");
         driver().findElement(By.cssSelector(".js-add-card")).click();
         String check = driver().findElement(By.cssSelector(".list-card-title")).getText();
+        List<TrelloList> resp = trelloApiClient.getBoardLists(boardId);
+        listId = resp.get(0).id;
+
         Assert.assertTrue(check.equals("azaza karto4ka"),"CARD CREATION FAILED");
     }
     @Test(dependsOnMethods = "createCard")
     public void commentCard(){
         driver().findElement(By.cssSelector(".list-card-details")).click();
         WebDriverWait wait = new WebDriverWait(driver(), 15);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".comment-box-input")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".js-new-comment-input")));
 
-        driver().findElement(By.cssSelector(".comment-box-input")).sendKeys("TOP COMMENT EVER");
+        driver().findElement(By.cssSelector(".js-new-comment-input")).sendKeys("TOP COMMENT EVER");
         driver().findElement(By.cssSelector(".js-add-comment")).click();
         driver().findElement(By.cssSelector(".js-close-window")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".badge-text")));
         String check = driver().findElement(By.cssSelector(".badge-text")).getText();
 
         Assert.assertTrue(check.equals("1"));
     }
 
     @Test (dependsOnMethods = "commentCard")
-    public void deleteCard() throws IOException {
-
-        driver().findElement(By.cssSelector(".ui-droppable.js-card-menu")).click();
+    public void deleteCard() throws IOException{
+        Actions actions = new Actions(driver());
+        WebElement cardBlock = driver().findElement(By.cssSelector(".ui-droppable"));
+        WebElement menuBlock = driver().findElement(By.cssSelector(".icon-edit"));
+        actions.moveToElement(cardBlock).moveToElement(menuBlock).click().perform();
+       // driver().findElement(By.cssSelector(".ui-droppable.js-card-menu")).click();
         List<WebElement> cardText = driver().findElements(By.cssSelector(".quick-card-editor-buttons-item-text"));
         for (WebElement text:cardText ) {
             if (text.getText().equals("Архивировать")){
                 text.click();
             }
         }
-        String boardId = "pYbUadku";
-        String idList = "59453dfb8b7226ede631b2a1";
-        TrelloApiClient trelloApiClient = new TrelloApiClient();
-        List<TrelloCard> checkeer = trelloApiClient.getAllCards(idList);
-        System.out.println(checkeer.size());
-        Assert.assertTrue(checkeer.size()==0,"ALL CARDS REMOVED");
+
+        List<TrelloCard> checkeer = trelloApiClient.getAllCards(listId);
+
+        Assert.assertTrue(checkeer.isEmpty(),"ALL CARDS REMOVED");
     }
 }
